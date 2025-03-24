4a7a91739008acffdec61f51f45a9035c06d5755
AyanamiSoryu <NikolskiyClarinet@gmail.com>
2025-03-24 21:46:30 +0400

    fixed copy-webpack-plugin for correct copy favicons from public to dist

--- webpack.config.mjs
+++ webpack.config.mjs
@@ -5,86 +5,88 @@ import CopyWebpackPlugin from "copy-webpack-plugin";
 
 
 export default {
-    mode: 'development',
-    entry: './src/index.tsx',
-    output: {
-        filename: 'main.js',
-        path: path.resolve('./dist/'),
-    },
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                use: 'ts-loader'
+  mode: 'development',
+  entry: './src/index.tsx',
+  output: {
+    filename: 'main.js',
+    path: path.resolve('./dist/'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader'
+      },
+      {
+        test: /.*\/src\/.*\.s?css$/,
+        use: [
+          "style-loader",
+          {
+            loader: "css-loader",
+            options: {
+              modules: true,
             },
-            {
-                test: /.*\/src\/.*\.s?css$/,
-                use: [
-                    "style-loader",
-                    {
-                        loader: "css-loader",
-                        options: {
-                            modules: true,
-                        },
-                    },
-                    "sass-loader",
-                ],
-            },
-            {
-                test: /.*\/src\/.*\.svg$/,
-                issuer: /\.[jt]sx?$/,
-                options: {
-                    replaceAttrValues: {
-                        black: 'currentColor',
-                        "#000": 'currentColor',
-                        "#202020": 'currentColor'
-                    }
-                },
-                loader: '@svgr/webpack',
-            },
-            {
-                test: /\.(woff|woff2|ttf|eot)$/,
-                // use: [
-                //     {
-                //         loader: 'file-loader',
-                //         // loader: 'file?name=src/css/[name].[ext]',
-                //         options: {
-                //             name: 'fonts/[name].[hash].[ext]',
-                //         },
-                //     },
-                // ],
-                type: 'asset/resource',
-            },
-            {
-                test: /\.(png|jpe?g|gif)$/i,
-                use: [
-                    {
-                        loader: 'file-loader',
-                    },
-                ],
-            },
+          },
+          "sass-loader",
         ],
-    },
-    devtool: 'eval-source-map',
-    resolve: {
-        extensions: ['.tsx', '.ts', '.js'],
-    },
-    plugins: [new HtmlWebpackPlugin({
-        title: "Custom title",
-        template: './public/index.html'
-    }),
-        new Dotenv() ,
-        new CopyWebpackPlugin({
-            patterns: [
-                { from: 'public', to: 'dist', globOptions: {
-                    ignore: [
-                      "./*.html",
-                      "./*.txt",
-                      "*.txt",
-                      "*.html"
-                    ]
-                    } }
-            ]
-        },),
+      },
+      {
+        test: /.*\/src\/.*\.svg$/,
+        issuer: /\.[jt]sx?$/,
+        options: {
+          replaceAttrValues: {
+            black: 'currentColor',
+            "#000": 'currentColor',
+            "#202020": 'currentColor'
+          }
+        },
+        loader: '@svgr/webpack',
+      },
+      {
+        test: /\.(woff|woff2|ttf|eot)$/,
+        // use: [
+        //     {
+        //         loader: 'file-loader',
+        //         // loader: 'file?name=src/css/[name].[ext]',
+        //         options: {
+        //             name: 'fonts/[name].[hash].[ext]',
+        //         },
+        //     },
+        // ],
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
+      },
     ],
+  },
+  devtool: 'eval-source-map',
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  plugins: [new HtmlWebpackPlugin({
+    title: "Custom title",
+    template: './public/index.html'
+  }),
+    new Dotenv() ,
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: 'public/', // Берём содержимое, но не саму папку
+          to: 'dist',
+          globOptions: {
+            ignore: [
+              '**/*.html', // Игнорируем все .html файлы
+              '**/*.txt'   // Игнорируем все .txt файлы
+            ]
+          }
+        }
+      ]
+    }),
+  ],
 };
