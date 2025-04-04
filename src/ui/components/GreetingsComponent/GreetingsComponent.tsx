6976c6886295bf1a4bde2710920c84566eab9a9b
AyanamiSoryu <NikolskiyClarinet@gmail.com>
2025-04-04 17:46:49 +0400

    Added my name at GreetingsComponent.tsx

--- src/ui/components/GreetingsComponent/GreetingsComponent.tsx
+++ src/ui/components/GreetingsComponent/GreetingsComponent.tsx
@@ -31,9 +31,9 @@ const GreetingsComponent = (props: { children?: JSX.Element }) => {
       <div className={classNames.children}>{children}</div>
       <h2 className={classNames.hello}>hello!</h2>
       <p className={classNames.greet}>
-        i’m a digital developer experienced in creating visually strong interfaces, websites and complex systems. i work
-        with modern instruments, analytical system interfaces, mobile and web applications and digital services for the
-        large businesses
+        my name is Ilia, i’m a digital developer experienced in creating visually strong interfaces, websites and
+        complex systems. i work with modern instruments, analytical system interfaces, mobile and web applications and
+        digital services for the large businesses
       </p>
       <div className={classNames.svg}>
         {links.map((link) => {
